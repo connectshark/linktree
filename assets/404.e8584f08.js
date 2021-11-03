b88140ea97a10d80edaf7bc39928eafc654c8664
connectshark <46649021+connectshark@users.noreply.github.com>
2021-11-03 09:42:23 +0000

    Deploying to gh-pages from @ connectshark/linktree@880c237a8c638c82eb9f5b7a49428490d0946ee9 🚀

---
+++ assets/404.e8584f08.js
@@ -0,0 +1,1 @@
+import{p as s,f as a,r as t,g as o,q as r,w as e,F as n,h as c,o as d,s as l}from"./vendor.2d1ce1bb.js";const u={};s("data-v-14385579");const f=c("h2",{class:"tip"},"沒這頁請回",-1),p=l("首頁");a(),u.render=function(s,a){const c=t("router-link");return d(),o(n,null,[f,r(c,{to:"/",class:"back"},{default:e((()=>[p])),_:1})],64)},u.__scopeId="data-v-14385579";export{u as default};
